486a575cd720effba5c6a6b34f88458f13665a3d
sheikhjubaer <mohammadjubaer01@gmail.com>
2023-09-26 15:03:47 +0600

    established error page for unwanted search

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    // errorElement: <ErrorPage></ErrorPage>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
